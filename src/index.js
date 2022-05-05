6f7e5af03e7b2c544985d92df010ada734cd4377
Elizabeth Nguyen <saper209@yahoo.com>
2022-05-04 22:26:58 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form")
+  const tasks = document.querySelector("#tasks")
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const inputValue = e.target.task.value
+    addElement(inputValue)
+  })
+
+  function addElement(element){
+    let p = document.createElement("p")
+    p.textContent = ` ${element} `
+    let button = document.createElement("button")
+    button.textContent = "x"
+    button.addEventListener("click", (e) => e.target.parentNode.remove())
+    p.appendChild(button)
+    tasks.appendChild(p)
+  }
+
 });
